0188ba7de95e892b659f0ba53357b8365d97b60e
Mira-st <orirekemi64@gmail.com>
2022-09-29 05:51:23 -0700

    More data Structures

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 9-multiple_by_2.py
+
+    """
+    Return a new dictionary with all values multipled by 2.
+    """
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
